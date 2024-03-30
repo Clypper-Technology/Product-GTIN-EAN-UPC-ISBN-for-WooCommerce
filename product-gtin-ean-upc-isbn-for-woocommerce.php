cb270f8ecb12dc5dbf91ad70b1c5495a69df7485
Casper Holten <casho@itu.dk>
2024-03-30 16:11:05 +0100

    Declare HPOS (High-Performance Order Storage) compatability

--- product-gtin-ean-upc-isbn-for-woocommerce.php
+++ product-gtin-ean-upc-isbn-for-woocommerce.php
@@ -17,6 +17,13 @@ if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly
 }
 
+add_action('before_woocommerce_init', function(){
+    if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+
+    }
+});
+
 /**
  * Currently plugin version.
  */
